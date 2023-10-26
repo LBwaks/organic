0fcf8b819a4ff82e425c4f32ad905de3c6340c6d
LBwaks <obwakuvictor@gmail.com>
2023-10-26 16:25:53 +0300

    add image

--- Products/forms.py
+++ Products/forms.py
@@ -6,34 +6,76 @@ from ckeditor.widgets import CKEditorWidget
 class ProductForm(forms.ModelForm):
     """Form definition for Product."""
 
+    images = forms.ImageField(
+        
+        widget=forms.ClearableFileInput(
+            attrs={"class": "form-control image", "required": True}
+        ),
+    )
+    tag = forms.ModelMultipleChoiceField(
+        widget=forms.SelectMultiple(
+            attrs={"class": "form-select tags", "required": True, "id": "product-id"}
+        ),
+        queryset=Tag.objects.all(),
+    )
+
     class Meta:
         """Meta definition for Productform."""
 
         model = Product
-        fields = ("title", "category", "tag", "description", "price", "quantity")
+        fields = ("title", "category",'tag',  "description", "price", "quantity")
         wigdets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "category": forms.Select(attrs={"class": "form-control"}),
+            "title": forms.TextInput(
+                attrs={"class": "form-control title", "required": True}
+            ),
+            "category": forms.Select(
+                attrs={"class": "form-select category", "required": True}
+            ),
             # "tag",
-            "description": CKEditorWidget(),
-            "price": forms.IntegerField(),
-            "quantity": forms.TextInput(attrs={"class": "form-control"}),
+            "description": CKEditorWidget(attrs={"required": True}),
+            "price": forms.TextInput(
+                attrs={"class": "form-control price", "required": True}
+            ),
+            "quantity": forms.TextInput(
+                attrs={"class": "form-control quantity", "required": True}
+            ),
         }
 
 
 class EditProductForm(forms.ModelForm):
     """Form definition for Product."""
 
+    images = forms.ImageField(
+        required=True,
+        widget=forms.ClearableFileInput(
+            attrs={"class": "form-control image", "required": True}
+        ),
+    )
+    tag = forms.ModelMultipleChoiceField(
+        widget=forms.SelectMultiple(
+            attrs={"class": "form-select tags", "required": True, "id": "product-id"}
+        ),
+        queryset=Tag.objects.all(),
+    )
+
     class Meta:
         """Meta definition for Productform."""
 
         model = Product
-        fields = ("title", "category", "tag", "description", "price", "quantity")
+        fields = ("title", "category",'tag', "description", "price", "quantity")
         wigdets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "category": forms.Select(attrs={"class": "form-control"}),
+            "title": forms.TextInput(
+                attrs={"class": "form-control title", "required": True}
+            ),
+            "category": forms.Select(
+                attrs={"class": "form-select category", "required": True}
+            ),
             # "tag",
-            "description": CKEditorWidget(),
-            "price": forms.IntegerField(),
-            "quantity": forms.TextInput(attrs={"class": "form-control"}),
+            "description": CKEditorWidget(attrs={"required": True}),
+            "price": forms.TextInput(
+                attrs={"class": "form-control price", "required": True}
+            ),
+            "quantity": forms.TextInput(
+                attrs={"class": "form-control quantity", "required": True}
+            ),
         }
--- Products/views.py
+++ Products/views.py
@@ -1,7 +1,7 @@
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -12,6 +12,7 @@ from django.views.generic import (
 from .models import Category, Tag, Product, ProductImage
 from .forms import ProductForm, EditProductForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -45,7 +46,12 @@ class ProductCreateView(CreateView):
     def form_valid(self, form):
         p = form.save(commit=False)
         p.user = self.request.user
+        images = self.request.FILES.getlist("images")
+        for image in images:
+            ProductImage.objects.create(product=p, image=image)
+            print(image)
         p.save()
+        
         return super(ProductCreateView, self).form_valid(form)
 
 
@@ -54,8 +60,41 @@ class ProductUpdateView(UpdateView):
     template_name = "products/update-product.html"
     form_class = EditProductForm
 
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.user != self.request.user:
+            messages.error(request, "You do not have permission to edit this product.")
+            return redirect(reverse("product-details", args=[obj.slug]))
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        p = form.save(commit=False)
+        images = self.request.FILES.getlist("images")
+        for image in images:
+            ProductImage.objects.update(image=image)
+        p.save_m2m()
+        return super().form_valid(form)
+
 
 class ProductDeleteView(DeleteView):
     model = Product
     template_name = "products/delete-product.html"
-    success_url = reverse_lazy('products')
+    success_url = reverse_lazy("products")
+    success_message = "Product Deleted"
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.user != self.request.user:
+            messages.error(
+                request, "You do not have permission to delete this product."
+            )
+            return redirect(reverse("product-details", args=[obj.slug]))
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
