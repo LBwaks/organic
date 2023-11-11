ab40693d662663c5696f73c28fcdc2c365e8cb0e
LBwaks <obwakuvictor@gmail.com>
2023-11-11 09:38:58 +0300

    static files collect

--- Organic/settings.py
+++ Organic/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
     # created applications
     "Products",
     "Cart",
-    'Pages',
-    'Account',
+    "Pages",
+    "Account",
 ]
 
 # crispy form
@@ -146,7 +146,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR / "media")
